9be6ceb7e3b9705a440f8982234c2d94b15139ab
SvitlanaVas <svetaklymchuk@gmail.com>
2023-05-14 19:45:45 +0300

    add 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) { 
+  event.preventDefault();
+  
+  const { email, password } = event.currentTarget.elements;
+  let dataObject = {};
+
+  if (!email.value || !password.value) {
+    alert('всі поля повинні бути заповнені');
+  } else {
+    dataObject = {
+      email: email.value,
+      password: password.value
+    }
+    
+    console.log(dataObject)
+  }
+  
+  formRef.reset();
+};
