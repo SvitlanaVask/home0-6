7ab99f68c864430e5c663c8ef243721c28a9262a
SvitlanaVas <svetaklymchuk@gmail.com>
2023-05-14 20:06:30 +0300

    add 8

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
+const bodyRef = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorPlaceHolder = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', onBgColorChange);
+
+function onBgColorChange() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorPlaceHolder.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
